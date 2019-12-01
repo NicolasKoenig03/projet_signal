fe577a94e0c24e613bca46f2bb32b4252037f568
KOENIG Nicolas <nkoenig@enseirb-matmeca.fr>
2019-12-01 20:08:47 +0100

    dev de la decomposition en trames

---
+++ bruitage.m
@@ -0,0 +1,15 @@
+function [signal_bruite] = bruitage( signal )
+%bruitage permet de bruiter un signal donné 
+varSignal = std2(signal)^2; % variance du bruit
+SNRdB = 5:5:15;
+for i=1:numel(SNRdB)
+  sigma_noise = sqrt(varSignal/10^(SNRdB(i)/10));
+  bruit = sigma_noise*randn(size(signal));
+  signal_bruite = signal+bruit; % using randn
+  signal_bruite = signal_bruite';   
+  figure,plot(signal_bruite)
+  title(['SNR = ' int2str(SNRdB(i)) 'dB' ...
+  ', \sigma_{noise} = ' num2str(sigma_noise)]);
+end
+end
+
--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-%% 
+
 close all, clear all, clc;
 load('fcno03fz.mat');
 load('fcno04fz.mat');
@@ -6,65 +6,78 @@ load('fcno04fz.mat');
 sigma = 10;
 mu = 0;
 var = sigma^2;
-N = 10000;
+N = length(fcno03fz);
 nfft = 512;
 fs = 8e3;
 Ts = 1/fs;
 
-t = [0 : Ts : N*Ts-1];
-f = [-1/2: 1/nfft : 1/2 - (1/nfft)];
+t = [0:Ts:N*Ts-Ts];
+f = [-1/2:1/nfft:1/2-(1/nfft)];
 
+
+%% Définition du bruit blanc gaussien centré et de sa DSP Théorique
 bbcg = mu+randn(1,N)*var;
-DSP = abs(fftshift(fft(bbcg))).^2/length(bbcg);
 
-corr_biased = xcorr(bbcg, 'biased');
-corr_unbiased = xcorr(bbcg, 'unbiased');
+% DSP Théorique
 theorique=mu^2*ones(1,2*N-1);
 theorique(1,floor(N-1)) = mu^2 + sigma^2;
-plot(fcno03fz/sum(fcno03fz))
+
+%% Définition des estimateurs de la fct d'autocorr 
+corr_biased = xcorr(bbcg, 'biased');
+corr_unbiased = xcorr(bbcg, 'unbiased');
+
+% plot(fcno03fz/sum(fcno03fz))
 
 t1 = -floor(length(corr_biased)/2): 1 :floor(length(corr_biased)/2);
 
+%% Spectre de puissance du bbcg & DSP du bruit
+spectre_bbcg = (abs(fft(bbcg)).^2); % en dB
+% DSP du bruit
+DSP_bruit = (pwelch(bbcg,[],[],[],nfft)); % en dB
+%Affichage  
+% subplot(211), plot(spectre_bbcg);
+% hold on, plot([0,length(bbcg)],[mean(spectre_bbcg) mean(spectre_bbcg)],'-.r');
+% subplot(212),plot(DSP_bruit)
+% hold on, plot([0, length(DSP_bruit)],[mean(DSP_bruit) mean(DSP_bruit)],'-.r');
+
+ 
 %% bruit
-sl = fcno03fz/sum(fcno03fz);
-
-RSB = 0.000001;
-
-sigma = sqrt(RSB)
-yl = sl + mu+randn(size(fcno03fz))*sigma;
-figure,
-plot([1:1:length(fcno03fz)],yl);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%% affichage 
-% vect_freq = linspace(0,pi,length(DSP));
-% plot(vect_freq,DSP);
-% title('DSP')
-% hold on, plot([vect_freq(1) vect_freq(end)],[sigma^2 sigma^2],'r');
-% hold on, plot([vect_freq(1) vect_freq(end)],[mean(DSP) mean(DSP)],'-.b');
+[signal_bruite1] = bruitage(fcno03fz);
+% [signal_bruite2] = bruitage(fcno04fz);
 % 
+% 
+% Periodogram & représentation temporelle
+% pour le signal non bruité
+figure,
+subplot(211)
+spectrogram(fcno03fz,[],[],N,fs,'yaxis')
+colorbar('off') 
+xlim([0.5,6])
+subplot(212)
+plot(t,fcno03fz)
+xlim([0.5,6])
+
+% pour le signal bruité
+figure,
+subplot(211)
+spectrogram(signal_bruite1,[],[],N,fs,'yaxis')
+colorbar('off')
+xlim([0.5,6])
+subplot(212)
+plot(t,signal_bruite1)
+xlim([0.5,6])
+%% affichage  
 % 
 % figure(2)
 % subplot(1,3,1)
 % plot(t1,corr_biased)
+% ylim([-1000,10e3]);
 % title('biaisé')
 % subplot(1,3,2)
 % plot(t1,corr_unbiased)
+% ylim([-1000,10e3]);
 % title('non biaisé')
 % subplot(1,3,3)
 % plot(t1, theorique);
-% ylim([-40,40]);
+% ylim([-0.1,1]);
 % title('théorique')
