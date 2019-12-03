72a1da72d23edb23d14b429b55af95986a12fc08
Choveton-Caillat julien <julien.choveton-caillat@bordeaux-inp.fr>
2019-12-03 12:46:45 +0100

    modif methods

--- Untitled.m
+++ Untitled.m
@@ -1,20 +1,21 @@
 close all, clear all, clc;
 load('fcno03fz.mat');
 load('fcno04fz.mat');
-sigma = 2;
+sigma = 4;
 mu = 0;
 var = sigma^2;
 N = 10000;
-nfft = 256;
+nfft = 1024;
 fs = 8e3;
 Ts = 1/fs;
 
 bbcg = mu+randn(1,N)*var;
 
 [y,f] = Bartlett(bbcg,nfft,fs);
-y1 = 10*log10(y);
+y1=y
 a = var*ones(1,length(f));
 figure
- plot(f,y1);
- hold on;
-plot(f,var*ones(1,length(y1)));
+plot(f,y1);
+hold on;
+
+plot(f,a);
